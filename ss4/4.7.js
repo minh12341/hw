a31fdccd1f3018f5f3bd7a84408d4e3a1fef03fe
minh12341 <61083690+minh12341@users.noreply.github.com>
2020-02-20 21:16:36 +0700

    Update 4.7.js

--- ss4/4.7.js
+++ ss4/4.7.js
@@ -4,16 +4,16 @@ let bmi = weight / Math.pow(height,2) ;
 if(bmi<18.5) {
     console.log("Dưới chuẩn") ; 
 }
-else if (bmi>=18.5 && bmi<=24.9) {
+else if (bmi>=18.5 && bmi<25) {
     console.log("Bình thường")  ;
 }
-else if ( bmi>=25 && bmi<= 29.9) {
+else if ( bmi>=25 && bmi< 30) {
     console.log("Thừa cân") ; 
 }
-else if (bmi>=30 && bmi<=34.9) {
+else if (bmi>=30 && bmi<35) {
     console.log("Béo phì cấp 1" ) ; 
 }
-else if (bmi>=35 && bmi<=39.9) {
+else if (bmi>=35 && bmi<40) {
     console.log("Béo phì cấp 2") ; 
 }
 else {
